7aeb2e624aa5f67ab8327b16723c009c67ef016a
Yaroslav <denisenkoforwork@gmail.com>
2025-11-03 17:19:55 +0200

    feat: add error result entity

---
+++ src/shared/Result.ts
@@ -0,0 +1,21 @@
+export const Ok = <T>(value: T): OkResult<T> => ({
+  ok: true,
+  value
+})
+
+export const Err = <T = never>(error: string): ErrResult => ({
+  ok: false,
+  error
+})
+
+export type Result<T> = OkResult<T> | ErrResult;
+
+export type OkResult<T> = {
+  ok: true,
+  value: T;
+}
+
+export type ErrResult = {
+  ok: false,
+  error: string;
+}
